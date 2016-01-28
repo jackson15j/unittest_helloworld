dbbec0f308c91cd4e19f43d64c17530d2b4c7137
Craig Astill <craig.astill@gmail.com>
2016-01-28 03:46:32 +0000

    Gearing up to make this a proper package.
    Getting ready to make it both an install-able and testable package.

---
+++ python/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Example unittests for Hello World in python.
+"""
+from setuptools import setup, find_packages
+setup(
+    name='unittest_helloworld',
+    description='Unittest Hello World in python.',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    dependency_links=[
+    ],
+    entry_points={
+    },
+    package_data={
+    },
+    author='Craig Astill',
+    author_email='craig.astill@gmail.com',
+    # url='https://github.com/jackson15j/unittest_helloworld/tree/master/python',
+)
