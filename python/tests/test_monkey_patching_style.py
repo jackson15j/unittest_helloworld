e7fae532c367530fb43edf1ff2e09441e2be23a6
Craig Astill <craig.astill@gmail.com>
2016-02-03 08:47:05 +0000

    Finished writing the Monkey Patch test docstring.

--- python/tests/test_monkey_patching_style.py
+++ python/tests/test_monkey_patching_style.py
@@ -3,34 +3,21 @@
 Monkey Patching style test
 ==========================
 
-TODO
-To unittest our `hello_world.py`, we need to make a change to our functional
-code so that it will be testable. We don't want to mess around trying to test
-the `print()` function since we can assume that as the first function that
-everyone uses in python, it has the most manual testing in the world (and
-hopefully a good batch of unittests in it's own right).
+How can we actually unittest our code without changing our program? After all,
+the`hello_world.py` program has been ground down to its most efficient form, so
+why add complexity to make it testable?
 
-So lets break the problem down:
+Putting on our "Dev" brain; we want to get inside the `print()` function and
+find out what it is going to print. Then we can see if what it is going to
+print is `"Hello World"` or not. So lets
+[Monkey Patch](https://stackoverflow.com/questions/5626193/what-is-a-monkey-patch)
+`print()`!
 
-* I want to print `"Hello World"`.
-
-Now the testing question can be worded as:
-
-* I want to test that `"Hello World"` is printed to stdout.
-* I want to test that `"Hello World"` will be printed to stdout.
-
-The second statement is the key here. We are trusting that `print()` will print
-an appropriate string (integer, list, whatever) to stdout. If we believe that
-the `print()` function will just "do it's job", then we can focus on:
-
-* Am I passing the string: "Hello World", to `print()`.
-
-Subtle, but this change in wording allows us to bring focus on what needs
-unittesting, as well as hints on how to change the functional code.
-
-The lest effort change to make the code testable, is to break out: `"Hello
-World"`, to a global string, then import our `hello_world.py` file into our
-unittest file.
+In reality it is easier to Monkey Patch `sys.stdout`, which `print()` is using
+(`print()` calls `sys.stdout.write()`). As long as we patch it with a file-like
+object we are golden. Do note that this will capture all `print()` calls until
+we undo the Monkey Patch. It will also cyclically blow up if you try this in an
+interpreter, but hey, no code changes to our main program at least.
 
 Benefits:
 ---------
@@ -42,10 +29,11 @@ Benefits:
 Drawbacks:
 ----------
 
-* We Monkey Patched `sys.stdout`! (Breaks all `print()` calls (uses `sys.stdout`) in our program 
-TODO
-* Had to change code to be testable (easy refactor in this case but may not be
-  trivial).
+* We Monkey Patched `sys.stdout`! (Breaks all `print()` calls (uses
+  `sys.stdout`) in our program until we undo the patching.
+* Do you **really** need to do Monkey Patching? It's trivial here, but can be
+  quite gnarly when the code gets more complex.
+* Is there a better way to make the program more testable?
 
 To run:
 -------
@@ -58,26 +46,26 @@ We should not see the assert exception if this is working.
 
 Assumptions:
 ------------
-TODO
+
 Like the other tests, this is a very quick & least effort way of
 unittesting. Here are the assumptions we made for a "good enough" unittest:
 
-* Moving `"hello world"` to a global means that we get the `print()` occurring
-  as an import side-effect (Not ideal and generally bad form). Like Monkey
-  Patching; if you need import side-effects in your module, make sure you
-  **really** need import side-effects.
-* Placing trust in `print()` functionality if we abide by it's API contract and
-  assuming it has been sufficiently tested by it's maintainer/provider.
+* Monkey Patching `sys.stdout` is a bit heavy handed as we swallow all calls to
+  it until the patching is undone. It also cyclically errors in interpreters.
+* We're only patching `sys.stdout` because we've investigated how `print()` is
+  implemented, to figure out how to mock it's behaviour to our needs. If the
+  internal structure of `print()` changes, our test will break.
+* The above two points show both big sweeping changes to Third Party code
+  behaviour and investigation effort into code outside of our programs realm.
+* From the design we are relying on import side-effects to execute the
+  `print()` function.
+* We step over any potential other `print()` calls by using `in` when checking
+  for `"Hello World"`.
 * Not using the unittest framework, so cannot run with nosetests.
 
-The key assumption here is that we trust people to maintain their own realm so
-that we can both test a portion of our internal logic in isolation, as well as
-the border to handing responsibility over to another function/module/party.
-
-However, without an integration test we may find out that `print()` is broken
-(does not print to stdout) despite what we are passing in is correct and all of
-our unittests are passing.
-
+The key assumption here is that we are doing something technically impressive
+to not change our existing code. However, we are digging into the bowls of a
+Third Party code base to meet our needs. At least it is a unittest though.
 """
 import sys
 import io
